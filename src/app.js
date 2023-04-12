78e8e4c7fcb3b3f9dd3ee51a6342b36a5a3c9490
guilhermefaj <figueiredoguiu@gmail.com>
2023-04-12 09:33:07 -0300

    Feat: Conexão de participantes ao db do MongoDB

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 import express from "express"
 import cors from "cors"
 import dayjs from "dayjs"
+import { MongoClient } from "mongodb"
+import dotenv from "dotenv"
 
 // Criação do App Servidor
 const app = express()
@@ -8,25 +10,33 @@ const app = express()
 // Configurações
 app.use(cors()) // Acessar a API em um front-end
 app.use(express.json()) // Os dados que trocarem com o cliente estarão em formato json
+dotenv.config()
 
 // Listas
-const participants = []
 const messages = []
+
+// Conexão com banco de dados
+let db
+const mongoClient = new MongoClient(process.env.DATABASE_URL)
+mongoClient.connect()
+    .then(() => db = mongoClient.db())
+    .catch((err) => console.log(err.message))
 
 // Endpoints
 app.post("/participants", (req, res) => {
     const { name } = req.body
 
-    const newUser = {
-        name,
-        lastStatus: Date.now()
-    }
-    participants.push(newUser)
-    res.send("OK")
+    const newUser = { name, lastStatus: Date.now() }
+
+    db.collection("participants").insertOne(newUser)
+        .then(() => res.sendStatus(201))
+        .catch((err) => res.status(500).send(err.message))
 })
 
 app.get("/participants", (req, res) => {
-    res.send(participants)
+    db.collection("participants").find().toArray()
+        .then(participants => res.send(participants))
+        .catch((err) => res.status(500).send(err.message))
 })
 
 app.post("/messages", (req, res) => {
