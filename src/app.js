066c26333685c554c5ae61be9bf6b8286974af75
guilhermefaj <figueiredoguiu@gmail.com>
2023-04-15 17:38:00 -0300

    Refactor: Conexão com o mongodb

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import cors from "cors"
 import dayjs from "dayjs"
 import { MongoClient, ObjectId } from "mongodb"
 import dotenv from "dotenv"
+import "dotenv/config"
 import joi from "joi"
 
 // Criação do App Servidor
@@ -13,14 +14,20 @@ app.use(cors()) // Acessar a API em um front-end
 app.use(express.json()) // Os dados que trocarem com o cliente estarão em formato json
 dotenv.config()
 
-// Listas
-
 // Conexão com banco de dados
-let db
-const mongoClient = new MongoClient(process.env.DATABASE_URL)
-mongoClient.connect()
-    .then(() => db = mongoClient.db())
-    .catch((err) => console.log(err.message))
+async function connectToDatabase() {
+    const mongoClient = new MongoClient(process.env.DATABASE_URL)
+    try {
+        await mongoClient.connect()
+        console.log("MongoDB conectado!")
+    } catch (err) {
+        console.log(err.message)
+    }
+    const db = mongoClient.db()
+    return db;
+}
+
+const db = await connectToDatabase();
 
 // Endpoints
 app.post("/participants", async (req, res) => {
@@ -63,10 +70,30 @@ app.get("/participants", async (req, res) => {
 
 app.post("/messages", async (req, res) => {
     const { to, text, type } = req.body
-    const from = req.headers.user
-    console.log(req.headers)
-    const newMessage = { from, to, text, type, time: dayjs().format('HH:mm:ss') }
+    const { user } = req.headers
+
+    const messagesSchema = joi.object({
+        to: joi.string().min(1).required(),
+        text: joi.string().min(1).required(),
+        type: joi.string().valid("message", "private_message").required(),
+    })
+
+    const validation = messagesSchema.validate(req.body, { abortEarly: false })
+
+    if (validation.error) {
+        const errors = validation.error.details.map(detail => detail.message)
+        return res.status(422).send(errors)
+    }
+
     try {
+        const existingParticipant = await db.collection("participants").findOne({ name: user })
+        if (!existingParticipant) {
+            return res.sendStatus(422)
+        }
+        const newMessage = { to, text, type }
+        newMessage.time = dayjs().format("HH:mm:ss")
+        newMessage.from = user
+
         await db.collection("messages").insertOne(newMessage)
         res.sendStatus(201)
     } catch (err) {
@@ -75,19 +102,49 @@ app.post("/messages", async (req, res) => {
 })
 
 app.get("/messages", async (req, res) => {
+    const limit = req.query.limit
+
+    const user = req.headers.user
+
+    if (limit !== undefined) {
+        if (isNaN(parseInt(limit)) || parseInt(limit) <= 0) {
+            return res.sendStatus(422);
+        }
+    }
+
     try {
-        const messages = await db.collection("messages").find().toArray()
+        const messages = await db.collection("messages").find({
+            $or: [
+                { to: "Todos" },
+                { to: user },
+                { from: user },
+                { type: "message" }
+            ]
+        })
+            .limit(parseInt(limit)).toArray()
         res.send(messages)
+
     } catch (err) {
         res.status(500).send(err.message)
     }
 })
 
-app.post("/status", (req, res) => {
+app.post("/status", async (req, res) => {
     const { user } = req.headers
 
     if (!user) {
         res.sendStatus(404)
+    }
+
+    try {
+        const existingParticipant = await db.collection("participants").findOne({ name: user })
+        if (!existingParticipant) {
+            return res.sendStatus(404)
+        }
+        await db.collection("participants").updateOne({ _id: existingParticipant._id }, { $set: { lastStatus: Date.now() } })
+        res.sendStatus(200)
+    } catch (err) {
+        res.status(500).send(err.message)
     }
 })
 
